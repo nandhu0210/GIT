e40d2f7c6e35b264ae59715b163b2457f3dc50c1
nandhu0210 <nandhinisaravanan99@gmail.com>
2024-11-22 15:47:32 +0530

    new push

---
+++ script1.js
@@ -0,0 +1,23 @@
+//nested array and object :
+
+var sclobj = [
+
+   {
+    name : "kalai",
+    age : 15,
+    std : "X"
+   } ,
+   {
+    name : "logan",
+    age : 13 ,
+    std : "v"
+   },
+   {
+      name : "sakthi",
+      age : 14,
+      std : "VI"
+
+   }
+
+]
+console.log(sclobj[1].name);
