71de2f66ab56cb7b24436bf64539e5edea267884
nandhu0210 <nandhinisaravanan99@gmail.com>
2024-11-22 15:29:17 +0530

    push

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/nandhu0210/GIT.git
-// git push -u origin main
+//Promise Concept
+
+function water() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const watfill = true;
+        if (watfill) {
+          resolve("watercan filled");
+        } else {
+          reject("water can not filled");
+        }
+      }, 3000);
+    });
+  }
+  
+  function room() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const rc = true;
+        if (rc) {
+          resolve("room cleaned");
+        } else {
+          reject("room not cleaned");
+        }
+      }, 2000);
+    });
+  }
+  function trash() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const to = false;
+        if (to) {
+          resolve("trash is out");
+        } else {
+          reject("trash not kept outside");
+        }
+      }, 500);
+    });
+  }
